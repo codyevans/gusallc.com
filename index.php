6faf0b11061dd4a0b6586767d12c25fa2b53fe57
Cody Evans <codyevansdesign@gmail.com>
2013-07-25 17:42:08 -0600

    initial terms page concept

--- index.php
+++ index.php
@@ -20,17 +20,15 @@
     </head>
     <body>
 
-        <h1>Garnet USA</h1>
+        <div class="terms-warning" id="terms-warning">
+            <p>
+                Please agree to our Terms and Conditions below to continue.
+            </p>
+        </div>
 
-        <p>Hello World</p>
+    
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        
+
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+// terms and conditons page
+
+$(window).on('load', function () {
+    
+	$('#terms-warning').delay(500).queue(function() {
+		$(this).addClass('warning-on');
+	});
+
+});
