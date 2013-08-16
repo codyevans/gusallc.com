c857bf771636aca7783dbcf2a32cc231f07c3e9f
Cody Evans <codyevansdesign@gmail.com>
2013-08-16 17:20:45 -0600

    fancy nav on scroll

--- index.php
+++ index.php
@@ -17,57 +17,170 @@
     </head>
     <body>
 
-        <div class="page">
+        <header class="main-header gpu">
 
-            <header class="main-header">
-                  
-               <div class="logo-wrap">
-                    <a href="/" title="Home">
+            <div class="header-inner">
+
+                <a href="/" class="logo-wrap gpu">
+                    <div class="logo-size gpu">
                         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                              width="139.25px" height="32px" viewBox="0 0 139.25 32" enable-background="new 0 0 139.25 32" xml:space="preserve">
-                        <g>
-                            <path class="logo" d="M24.825,11.242h2.935c1.487,0,2.776,0.534,3.855,1.601c1.113,1.075,1.67,2.364,1.67,3.883v8.327
-                                c0,1.814-0.796,3.286-2.39,4.415c-1.564,1.067-3.424,1.602-5.574,1.602H6.506c-1.775,0-3.149-0.589-4.125-1.754
-                                c-0.919-1.19-1.38-2.616-1.38-4.263V7.297c0-1.944,0.769-3.493,2.302-4.651c1.441-1.144,3.34-1.715,5.687-1.715h21.746v6.81H9.467
-                                v16.854h15.358v-8.853h-3.334L24.825,11.242z"/>
-                            <path class="logo" d="M60.158,0.931h8.462v24.167c0,1.798-0.798,3.264-2.393,4.37c-1.547,1.067-3.401,1.602-5.572,1.602H41.861
-                                c-1.806,0-3.188-0.589-4.148-1.754c-0.92-1.19-1.381-2.616-1.381-4.263V0.931h8.464v23.663h15.362V0.931z"/>
-                            <path class="logo" d="M97.2,11.242c2.045,0.121,3.577,0.702,4.604,1.752c1.02,1.039,1.533,2.464,1.533,4.279v7.778
-                                c0,1.921-0.789,3.423-2.369,4.507c-1.594,1.007-3.458,1.511-5.595,1.511H71.053v-6.476h23.826v-6.877H77.598
-                                c-2.243-0.03-3.893-0.503-4.954-1.435c-1.055-0.921-1.591-2.447-1.591-4.568V7.297c0-1.898,0.771-3.454,2.304-4.651
-                                c1.502-1.144,3.393-1.715,5.683-1.715h21.837v6.81H79.512v3.501H97.2z"/>
-                            <path class="logo" d="M114.253,17.717v13.353h-8.479V7.297c0-1.898,0.779-3.454,2.348-4.651c1.456-1.144,3.352-1.715,5.683-1.715
-                                h18.794c1.699,0,3.037,0.579,4.016,1.738c1.012,1.038,1.511,2.403,1.511,4.08v24.321h-8.508V17.717H114.253z M129.616,7.84h-15.363
-                                v3.478h15.363V7.84z"/>
-                        </g>
+                            <g>
+                                <path class="logo" d="M24.825,11.242h2.935c1.487,0,2.776,0.534,3.855,1.601c1.113,1.075,1.67,2.364,1.67,3.883v8.327
+                                    c0,1.814-0.796,3.286-2.39,4.415c-1.564,1.067-3.424,1.602-5.574,1.602H6.506c-1.775,0-3.149-0.589-4.125-1.754
+                                    c-0.919-1.19-1.38-2.616-1.38-4.263V7.297c0-1.944,0.769-3.493,2.302-4.651c1.441-1.144,3.34-1.715,5.687-1.715h21.746v6.81H9.467
+                                    v16.854h15.358v-8.853h-3.334L24.825,11.242z"/>
+                                <path class="logo" d="M60.158,0.931h8.462v24.167c0,1.798-0.798,3.264-2.393,4.37c-1.547,1.067-3.401,1.602-5.572,1.602H41.861
+                                    c-1.806,0-3.188-0.589-4.148-1.754c-0.92-1.19-1.381-2.616-1.381-4.263V0.931h8.464v23.663h15.362V0.931z"/>
+                                <path class="logo" d="M97.2,11.242c2.045,0.121,3.577,0.702,4.604,1.752c1.02,1.039,1.533,2.464,1.533,4.279v7.778
+                                    c0,1.921-0.789,3.423-2.369,4.507c-1.594,1.007-3.458,1.511-5.595,1.511H71.053v-6.476h23.826v-6.877H77.598
+                                    c-2.243-0.03-3.893-0.503-4.954-1.435c-1.055-0.921-1.591-2.447-1.591-4.568V7.297c0-1.898,0.771-3.454,2.304-4.651
+                                    c1.502-1.144,3.393-1.715,5.683-1.715h21.837v6.81H79.512v3.501H97.2z"/>
+                                <path class="logo" d="M114.253,17.717v13.353h-8.479V7.297c0-1.898,0.779-3.454,2.348-4.651c1.456-1.144,3.352-1.715,5.683-1.715
+                                    h18.794c1.699,0,3.037,0.579,4.016,1.738c1.012,1.038,1.511,2.403,1.511,4.08v24.321h-8.508V17.717H114.253z M129.616,7.84h-15.363
+                                    v3.478h15.363V7.84z"/>
+                            </g>
                         </svg>
-                    </a>
-               </div> <!-- /logo -->
+                    </div> 
+                </a> <!-- /logo -->
                 
-                <!-- <nav>
-                    <a href="#" class="current">About</a>
+                <nav class="gpu">
+                    <a href="#">About</a>
                     <a href="#">Team</a>
                     <a href="#">Products</a>
                     <a href="#">Contact</a>
-                    <a href="#" class="current">Documents</a> 
-                </nav> -->
+                    <a href="#">Documents</a> 
+                </nav>
 
-            </header> <!-- /main nav -->
+            </div> <!-- /header-inner -->
 
-            <a id="simple-menu" href="#sidr">Toogle menu</a>
- 
-            <div id="sidr">
-              <!-- Your content -->
-              <ul>
-                <li><a href="#">List 1</a></li>
-                <li class="active"><a href="#">List 2</a></li>
-                <li><a href="#">List 3</a></li>
-              </ul>
-            </div>
+        </header> 
 
+        <div class="container">
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+
+
+
+        </div>
 
         
-        </div> <!-- /page -->
+
+
+
+
+       <!--  <a id="simple-menu" href="#sidr">Toogle menu</a> -->
+
+        <div id="sidr">
+          <!-- Your content -->
+          <ul>
+            <li><a href="#">List 1</a></li>
+            <li class="active"><a href="#">List 2</a></li>
+            <li><a href="#">List 3</a></li>
+          </ul>
+        </div>
+
+    
+    
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 
-// terms and conditons page
-
-/*
-
-$(window).on('load', function () {
-    
-	$('#terms-warning').delay(500).queue(function() {
-		$(this).addClass('warning-on');
-	});
+//add remove scroll on header
+$(window).scroll(function(){
+	
+	if($(window).scrollTop() <= 20){
+	  $('.main-header').removeClass('scroll')
+	}else{
+	  $('.main-header').addClass('scroll')
+	}
 
 });
 
-*/
-
 
 // sidr nav
-
 $('#simple-menu').sidr();
 
 $('#close').sidr();
