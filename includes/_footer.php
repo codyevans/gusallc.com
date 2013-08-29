563640bf2a4a750c8bff0669e8e720e11788cc68
Cody Evans <codyevansdesign@gmail.com>
2013-08-28 22:52:19 -0600

    making includes for structure

---
+++ includes/_footer.php
@@ -0,0 +1,16 @@
+        <div class="footer-reveal gusa-footer">
+            <footer>
+                <div class="footer-wrap">
+                    <h2>Garnet USA</h2>
+                    <p>2200 MT Hwy 287 | Alder, Montana 59710</p>
+                    <p class="copyright">© Copyright <?php echo date('Y'); ?> Garnet USA, LLC - All Rights Reserved</p>
+                </div>
+            </footer>
+        </div> <!-- /footer -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ includes/_header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <title>Garnet USA</title>
+
+        <meta name="description" content="Garnet Mine Located in Alder, Montana">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header class="main-header gpu">
+
+            <div class="header-inner">
+
+                <a href="/" class="logo-wrap gpu">
+                    <div class="logo-size gpu">
+                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                             width="139.25px" height="32px" viewBox="0 0 139.25 32" enable-background="new 0 0 139.25 32" xml:space="preserve">
+                            <g>
+                                <path class="logo" d="M24.825,11.242h2.935c1.487,0,2.776,0.534,3.855,1.601c1.113,1.075,1.67,2.364,1.67,3.883v8.327
+                                    c0,1.814-0.796,3.286-2.39,4.415c-1.564,1.067-3.424,1.602-5.574,1.602H6.506c-1.775,0-3.149-0.589-4.125-1.754
+                                    c-0.919-1.19-1.38-2.616-1.38-4.263V7.297c0-1.944,0.769-3.493,2.302-4.651c1.441-1.144,3.34-1.715,5.687-1.715h21.746v6.81H9.467
+                                    v16.854h15.358v-8.853h-3.334L24.825,11.242z"/>
+                                <path class="logo" d="M60.158,0.931h8.462v24.167c0,1.798-0.798,3.264-2.393,4.37c-1.547,1.067-3.401,1.602-5.572,1.602H41.861
+                                    c-1.806,0-3.188-0.589-4.148-1.754c-0.92-1.19-1.381-2.616-1.381-4.263V0.931h8.464v23.663h15.362V0.931z"/>
+                                <path class="logo" d="M97.2,11.242c2.045,0.121,3.577,0.702,4.604,1.752c1.02,1.039,1.533,2.464,1.533,4.279v7.778
+                                    c0,1.921-0.789,3.423-2.369,4.507c-1.594,1.007-3.458,1.511-5.595,1.511H71.053v-6.476h23.826v-6.877H77.598
+                                    c-2.243-0.03-3.893-0.503-4.954-1.435c-1.055-0.921-1.591-2.447-1.591-4.568V7.297c0-1.898,0.771-3.454,2.304-4.651
+                                    c1.502-1.144,3.393-1.715,5.683-1.715h21.837v6.81H79.512v3.501H97.2z"/>
+                                <path class="logo" d="M114.253,17.717v13.353h-8.479V7.297c0-1.898,0.779-3.454,2.348-4.651c1.456-1.144,3.352-1.715,5.683-1.715
+                                    h18.794c1.699,0,3.037,0.579,4.016,1.738c1.012,1.038,1.511,2.403,1.511,4.08v24.321h-8.508V17.717H114.253z M129.616,7.84h-15.363
+                                    v3.478h15.363V7.84z"/>
+                            </g>
+                        </svg>
+                    </div> 
+                </a> <!-- /logo -->
+
+                <a href="#" class="menu-icon" id="mobile-nav-btn">
+                    <div class="icon-size">
+                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                 width="28.951px" height="26.693px" viewBox="0 0 28.951 26.693" enable-background="new 0 0 28.951 26.693" xml:space="preserve">
+                            <g>
+                                <rect class="navicon" x="0.913" y="1.02" width="27.345" height="6"/>
+                                <rect class="navicon" x="0.913" y="10.458" width="27.345" height="6"/>
+                                <rect class="navicon" x="0.913" y="19.896" width="27.345" height="6"/>
+                            </g>
+                        </svg>
+                    </div>
+                </a>
+
+                <nav class="gpu"> 
+                    <a href="#">About</a>
+                    <a href="#" class="current">Team</a>
+                    <a href="#">Products</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Documents</a>
+                </nav>
+
+            </div> <!-- /header-inner -->
+        </header>
